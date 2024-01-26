34d899b812b859c27b421644d9e459ee1d5e1476
Parsa Hosseini <officer47p@gmail.com>
2024-01-26 20:15:00 +0330

    A simple text and button

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	a := app.New()
+	w := a.NewWindow("Open Wallet")
+	w.Resize(fyne.NewSize(300, 300))
+
+	introduction := canvas.NewText("Hello Fyne!", nil)
+	introduction.TextSize = 24
+	introduction.Alignment = fyne.TextAlignCenter
+	// introduction
+	// introduction.
+	// introduction.Alignment = fyne.TextAlignCenter
+	// introduction.TextStyle
+	w.SetContent(container.NewVBox(
+		introduction,
+		widget.NewButton("Hi!", func() {
+			introduction.Text = "Welcome :)"
+			introduction.Refresh()
+		}),
+	))
+
+	w.ShowAndRun()
 }
